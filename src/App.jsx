dc44d0bddd2d16778c7f0070ba380013b1bddf62
ryo <ryo3brue@gmail.com>
2023-03-20 21:17:01 +0900

    add leaflet mapp file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Form from "./Form";
+import RestaurantList from "./RestaurantList";
+import LeafletMap from "./LeafletMap";
 
 function App() {
   return (
     <>
       <Form />
       <RestaurantList />
+      <LeafletMap />
     </>
   );
 }
