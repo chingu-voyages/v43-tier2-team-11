215dfd988c9ff90be649cd9dd6d227f8deabbdd1
Sam <uizyk@gmail.com>
2023-03-22 11:15:58 +0900

    update structure to fit new design

--- src/components/RestaurantDetail/RestaurantDetail.jsx
+++ src/components/RestaurantDetail/RestaurantDetail.jsx
@@ -1,58 +1,37 @@
 import React from 'react';
 import './RestaurantDetail.css';
 
+// Font awesome imports for icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+// import { faPlateUtensils } from '@fortawesome/free-solid-svg-icons';
+
+
 const RestaurantDetail = ({ restaurant }) => {
   const mockData = {
-    name: "The Spicy Spoon",
+    title: "The Spicy Spoon",
     image: "https://example.com/spicy-spoon.jpg",
-    options: ["Dine-in", "Take-out", "Delivery"],
+    type: "Fast Food",
     address: "123 Main Street, Anytown USA",
-    openingTime: "10:00",
-    closingTime: "21:00",
-    menuLink: "https://example.com/menu.pdf",
-    phoneNumber: "555-123-4567"
+    phoneNumber: "555-123-4567",
+    reviewStar: 4.5,
+    price: "$$"
   };
 
-  const { name, options, address, openingTime, closingTime, menuLink, phoneNumber } = mockData;
-  const currentTime = new Date();
-  const openingHour = new Date(currentTime);
-  openingHour.setHours(openingTime.slice(0, 2), openingTime.slice(3), 0); // set opening time
-  const closingHour = new Date(currentTime);
-  closingHour.setHours(closingTime.slice(0, 2), closingTime.slice(3), 0); // set closing time
-
-  let isOpen = false;
-  let openOrClosed = '';
-  let nextOpenTime = '';
-
-  if (currentTime >= openingHour && currentTime <= closingHour) {
-    isOpen = true;
-    openOrClosed = 'Open';
-  } else {
-    const nextOpeningDay = currentTime.getDay() === 6 ? 0 : currentTime.getDay() + 1; // set next opening day (Mon-Sun)
-    const nextOpeningDate = new Date(currentTime);
-    nextOpeningDate.setDate(currentTime.getDate() + (nextOpeningDay - currentTime.getDay()));
-    nextOpeningDate.setHours(openingTime.slice(0, 2), openingTime.slice(3), 0); // set next opening time
-    const timeDiff = nextOpeningDate.getTime() - currentTime.getTime();
-    const hours = Math.floor(timeDiff / 3600000);
-    const minutes = Math.floor((timeDiff - hours * 3600000) / 60000);
-    const nextOpeningDayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][nextOpeningDay];
-    nextOpenTime = `at ${openingTime} AM ${nextOpeningDayName}`;
-    openOrClosed = 'Closed';
-  }
+  const { title, type, address, phoneNumber, reviewStar, price } = mockData;
 
   return (
     <div className="restaurant-detail">
-      <h1>{name}</h1>
-      <p>Service options: {options.join(', ')}</p>
-      <p>Address: {address}</p>
-      <p>Hours: {openOrClosed} - {isOpen ? `Closes at ${closingTime}` : `Opens ${nextOpenTime}`}</p>
-      <p>Menu: <a href={menuLink}>{menuLink}</a></p>
-      <p>Phone: {phoneNumber}</p>
-      <h3>Review Summary</h3>
+      <h1>{title}</h1>
+      <p>{reviewStar} stars</p>
+      <p>{price}</p>
+      <p><FontAwesomeIcon icon={faLocationDot} /> {address}</p>
+      <p><FontAwesomeIcon icon={faPhone} /> {phoneNumber}</p>
+      {/* <p><FontAwesomeIcon icon={faPlateUtensils} /> {type}</p> */}
+      <button>SAVE TO MY LIST</button>
     </div>
   );
 };
 
-
 export default RestaurantDetail;
-
