6a573b4737fb2a62bdb453cb0e2c22113d70e845
ryo <ryo3brue@gmail.com>
2023-03-22 17:24:53 +0900

    set 404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,26 @@
 import Navigation from "../components/layout/Navigation";
 import Footer from "../components/layout/Footer";
+import { Link } from "react-router-dom";
+import classes from "./ErrorPage.module.scss";
 
 const ErrorPage = () => {
   return (
-    <>
+    <div className={classes.errorPage}>
       <Navigation />
-      <h1>Error Page(Ryo)</h1>
+      <h1>404 NOT FOUND</h1>
+      <br />
+      <h2>Here are some helpful links:</h2>
+      <Link to="/map" className={classes.link}>
+        Map Page
+      </Link>
+      <br />
+      <br />
+      <Link to="/form" className={classes.link}>
+        Form Page
+      </Link>
+
       <Footer />
-    </>
+    </div>
   );
 };
 
