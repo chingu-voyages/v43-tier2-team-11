2f44f7c91ed02b3f4d2acb9af45e76d748db3a86
Khemmie-Ray <atokemmy@gmail.com>
2023-03-25 10:01:12 +0100

    update to footer and link in nav

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,9 @@
+import classes from "./Footer.module.scss"
+
 const Footer = () => {
   return (
     <div className={classes.footer}>
-      <p>Copyright &copy; 2023 4Foodies. All rights reserved</p>
+      <p>Copyright &copy; 2023 4Foodies. All rights reserved.</p>
     </div>
   );
 };
