312aad197b568d1f607ad9ebec39e31c563e684f
Khemmie-Ray <atokemmy@gmail.com>
2023-03-25 09:01:13 +0100

    footer corrections

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import LandingPage from "./pages/LandingPage";
 import ErrorPage from "./pages/ErrorPage";
 import MapPage from "./pages/MapPage";
 import Form from "./components/Form";
-
-//import RestaurantDetail from "./components/RestaurantDetail/RestaurantDetail";
 import RestaurantCard from "./components/RestaurantCard/RestaurantCard";
 
 const router = createBrowserRouter([
@@ -26,10 +24,6 @@ const router = createBrowserRouter([
       {
         path: "/form",
         element: <Form />,
-      },
-      {
-        path: "/restaurantDetail",
-        element: <RestaurantDetail />,
       },
       {
         path: "/restaurantCard",
