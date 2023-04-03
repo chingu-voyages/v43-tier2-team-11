fbf42769b33a11476be8028aa69ac0db4cde8b74
Emerson Bualat <96451055+emerson-bualat@users.noreply.github.com>
2023-04-03 10:16:28 +0800

    Update landing page styling and footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,36 +2,41 @@
 import { useRef, useState } from "react";
 import Navigation from "../components/layout/Navigation";
 import Footer from "../components/layout/Footer";
-import heroImg from "../assets/illustrations/1.svg"
-import classes from "./LandingPage.module.scss"
-import Form from "../components/Form"
-
+import heroImg from "../assets/illustrations/1.svg";
+import classes from "./LandingPage.module.scss";
+import Form from "../components/Form";
 
 const LandingPage = () => {
-  const [showForm, setShowForm] = useState(false)
+  const [showForm, setShowForm] = useState(false);
 
   const renderForm = () => {
-    setShowForm(!showForm)
-  }
+    setShowForm(!showForm);
+  };
 
   return (
-    <div className={classes.mainContainer}>
+    <>
       <Navigation />
-      <div className={classes.home}>
-        <div className={classes.hero}>
-      <img src={heroImg} alt="" />
-      </div>
-      <div className={classes.homeContent}>
-      <h1 className={classes.homeTitle}>Find the Best Restaurants with Ease</h1>
-      <p>4 Foodies is a user-friendly restaurant finder website that helps you find the best places</p>
-      <button onClick={renderForm}>Explore Locations</button>
-      {showForm && <div>
-      <Form />
-    </div>}
-      </div>
-      </div>
+      <header className={classes.hero}>
+        <div className={classes.heroContainer}>
+          <article className={classes.heroImage}>
+            <img src={heroImg} alt="" />
+          </article>
+          <article className={classes.heroInfo}>
+            <h1>Find the Best Restaurants with Ease</h1>
+            <p>
+              4 Foodies is a user-friendly restaurant finder website that helps
+              you find the best places
+            </p>
+            <button onClick={renderForm}>Explore Locations</button>
+            {showForm && (
+              <div>
+                <Form />
+              </div>
+            )}
+          </article>
+        </div>
 
-      {/* <p>(For development purpose links to each pages are below)</p>
+        {/* <p>(For development purpose links to each pages are below)</p>
       <ul>
         <li>
           <Link to="/map">Map Page</Link>
@@ -43,9 +48,9 @@ const LandingPage = () => {
           <Link to="/form">Form Page</Link>
         </li>
       </ul> */}
-
+      </header>
       <Footer />
-    </div>
+    </>
   );
 };
 
