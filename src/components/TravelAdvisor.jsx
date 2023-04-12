8f8e8bb8761f7f9acf42d4e762957d4ce5a7d62c
Ryo Izawa <101433518+ryoizawa@users.noreply.github.com>
2023-04-12 13:53:23 +0900

    Deleted console.log
    Deleted 2 console.log

--- src/components/TravelAdvisor.jsx
+++ src/components/TravelAdvisor.jsx
@@ -20,13 +20,11 @@ const TravelAdvisor = () => {
     axios
       .request(options)
       .then(function (response) {
-        console.log(response.data);
-        // console.log(
-        //   response.data.data.AppPresentation_queryAppListV2[0].sections
-        // );
+       
+  
       })
       .catch(function (error) {
-        console.error(error);
+    
       });
   };
 
