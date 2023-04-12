d7d9240ae1459bc61c40f8cc5ac3ebbfea071e5b
TKS9988 <div.contact.mail@gmail.com>
2023-04-12 21:39:30 +0900

    fix: nuction

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,93 +1,93 @@
-import { useEffect, useState } from "react"
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet"
-import { useLocation } from "react-router"
-import { Link } from "react-router-dom"
-import Loading from "./Loading"
-import FormInput from "./FormInput"
-import Navigation from "./layout/Navigation"
-import Footer from "./layout/Footer"
-import fullStar from "../assets/rating_icons/full-star.svg"
-import halfStar from "../assets/rating_icons/half-star.svg"
-import { getMapData } from "./services/api"
-import classes from "./Map.module.scss"
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import Loading from "./Loading";
+import FormInput from "./FormInput";
+import Navigation from "./layout/Navigation";
+import Footer from "./layout/Footer";
+import fullStar from "../assets/rating_icons/full-star.svg";
+import halfStar from "../assets/rating_icons/half-star.svg";
+import { getMapData } from "./services/api";
+import classes from "./Map.module.scss";
 
 const Map = () => {
-  const LENGTHSIZE = 35
-  const { state } = useLocation([])
-  const [mapData, setMapData] = useState([])
-  const [currentLocation, setCurrentLocation] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [initialLoading, setInitialLoading] = useState(true)
-  const [coords, setCoords] = useState([])
-  const [changedCoords, setChangedCoords] = useState([])
-  const mapZoomLevel = 15
+  const LENGTHSIZE = 35;
+  const { state } = useLocation([]);
+  const [mapData, setMapData] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(true);
+  const [coords, setCoords] = useState([]);
+  const [changedCoords, setChangedCoords] = useState([]);
+  const mapZoomLevel = 15;
 
   // - current -
   useEffect(() => {
     if (!loading && state === null) {
       getMapData(coords).then((res) => {
         setMapData(res)
-      })
+      });
     } else {
       return
-    }
-  }, [loading, coords, state])
+    };
+  }, [loading, coords, state]);
 
   useEffect(() => {
     if (state === null) {
-      return navigator.geolocation.getCurrentPosition(location)
-    }
-  }, [state])
+      return navigator.geolocation.getCurrentPosition(location);
+    };
+  }, [state]);
 
   useEffect(() => {
     if (!currentLocation && state === null) {
-      setLoading(true)
+      setLoading(true);
     } else {
       const { lat, long } = currentLocation;
-      setCoords([lat, long])
-      setChangedCoords([lat, long])
-      setLoading(false)
-      setInitialLoading(false)
-    }
-  }, [currentLocation, state])
+      setCoords([lat, long]);
+      setChangedCoords([lat, long]);
+      setLoading(false);
+      setInitialLoading(false);
+    };
+  }, [currentLocation, state]);
 
   const location = (position) => {
-    let location = { 'lat': position.coords.latitude, 'long': position.coords.longitude }
-    setCurrentLocation(location)
-  }
+    let location = { 'lat': position.coords.latitude, 'long': position.coords.longitude };
+    setCurrentLocation(location);
+  };
   // - current end -
 
   // - search data -
   useEffect(() => {
     if (state !== null) {
       const { latitude, longitude } = state['suggestedLocations'];
-      setCoords([latitude, longitude])
-      setChangedCoords([latitude, longitude])
-      setMapData(state['searchData'])
-      setLoading(false)
+      setCoords([latitude, longitude]);
+      setChangedCoords([latitude, longitude]);
+      setMapData(state['searchData']);
+      setLoading(false);
     } else {
-      return
-    }
-  }, [state])
+      return;
+    };
+  }, [state]);
   // - search data end -
 
   // - map -
   const markers = mapData.map((el, index) => {
-    const { latitude, longitude } = el['geocodes']['main']
+    const { latitude, longitude } = el['geocodes']['main'];
     const coords = [latitude, longitude];
     return (
       <Marker position={coords} key={index}>
         <Popup>{el['name']}</Popup>
       </Marker>
-    )
-  })
+    );
+  });
 
   const ChangeMapCenter = ({ position }) => {
-    const map = useMap()
-    map.panTo(position)
-    return null
-  }
-  const initialCenter = coords
+    const map = useMap();
+    map.panTo(position);
+    return null;
+  };
+  const initialCenter = coords;
   // - map end -
 
   return (
