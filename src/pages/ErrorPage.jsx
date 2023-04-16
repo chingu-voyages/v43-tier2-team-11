173732b2fea46a52cc8dc48e1faa8eb9944dfcaf
Emerson Bualat <96451055+emerson-bualat@users.noreply.github.com>
2023-04-16 16:55:26 +0800

    Add nav and footer component with styling

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,28 +1,32 @@
 import { Link } from "react-router-dom";
 import classes from "./ErrorPage.module.scss";
 import PageNotFound from "../assets/illustrations/404icon.svg";
-
+import Navigation from "../components/layout/Navigation";
+import Footer from "../components/layout/Footer";
 const ErrorPage = () => {
   const handleClick = () => {
     window.location.href = "/";
   };
 
   return (
-    <div className={classes.errorPage}>
-      <div className={classes.descriptions}>
-        <img src={PageNotFound} alt="Oops! Page Not Found" />
-        <div className={classes.title}>Oops! Page Not Found</div>
-        <br />
-        <div className={classes.text}>
-          We're sorry, but the page you are looking for could not be found. It's
-          possible that the page has been removed, renamed, or is temporarily
-          unavailable.
+    <>
+      <Navigation />
+      <div className={classes["error-page"]}>
+        <div className={classes["error-page-container"]}>
+          <img src={PageNotFound} alt="Oops! Page Not Found" />
+          <div className={classes["error-page-info"]}>
+            <h3>Oops! Page Not Found</h3>
+            <p>
+              We're sorry, but the page you are looking for could not be found.
+              It's possible that the page has been removed, renamed, or is
+              temporarily unavailable.
+            </p>
+            <button onClick={handleClick}>go back to home</button>
+          </div>
         </div>
-        <button className={classes.button} onClick={handleClick}>
-          go back to home
-        </button>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
